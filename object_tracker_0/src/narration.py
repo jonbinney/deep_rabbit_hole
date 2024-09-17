938bb55f01a6bde8eae2e334b9b94e58696560a2
Julian Cerruti <julian@inorbit.ai>
2024-09-17 17:23:20 -0300

    WIP: narration with llama

---
+++ object_tracker_0/src/narration.py
@@ -0,0 +1,28 @@
+"""
+Turn a list of actions into a narration.
+
+NOTE: In order to run this, you first need to authenticate with the Hugging Face API.
+NOTE: The first time you run this, it will downoad several gigabytes of model data.
+"""
+import transformers
+import torch
+
+model_id = "meta-llama/Meta-Llama-3.1-8B-Instruct"
+
+pipeline = transformers.pipeline(
+    "text-generation",
+    model=model_id,
+    model_kwargs={"torch_dtype": torch.bfloat16},
+    device_map="auto",
+)
+
+messages = [
+    {"role": "system", "content": "You are a pirate chatbot who always responds in pirate speak!"},
+    {"role": "user", "content": "Who are you?"},
+]
+
+outputs = pipeline(
+    messages,
+    max_new_tokens=256,
+)
+print(outputs[0]["generated_text"][-1])
