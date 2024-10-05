ecd8bce24f1615a46b54f2c8bc0d89978c15a070
Jon Binney <jon.binney@gmail.com>
2024-10-05 17:54:58 -0400

    Save live video to files

--- camera_control/src/camera_control/gstreamer_camera_capture.py
+++ camera_control/src/camera_control/gstreamer_camera_capture.py
@@ -1,5 +1,7 @@
+import datetime
 import gi
 import numpy as np
+import pathlib
 
 gi.require_version('Gst', '1.0')
 from gi.repository import Gst
@@ -13,10 +15,13 @@ class GStreamerCameraCapture:
     """
     def __init__(self, camera_uri):
         Gst.init(None)
+        home_dir = pathlib.Path.home()
+        datetime_str = datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%Z")
         pipeline_str = " " .join([
-            f"rtspsrc location={camera_uri} latency=30 ! rtph265depay ! h265parse",
-            " ! avdec_h265 ! videoconvert ! video/x-raw,format=BGR ! appsink name=appsink"
-             ])
+            f'rtspsrc location={camera_uri} latency=30 ! rtph265depay ! h265parse ! tee name=t ',
+            ' t. ! queue ! avdec_h265 ! videoconvert ! video/x-raw,format=BGR ! appsink name=appsink ',
+            f' t. ! queue ! splitmuxsink max-size-time=3600000000000 location={home_dir}/Downloads/rabbits_{datetime_str}_%02d.mp4',
+            ])
         print('Creating gstreamer pipeline: {}'.format(pipeline_str))
         self.pipeline = Gst.parse_launch(pipeline_str)
             
