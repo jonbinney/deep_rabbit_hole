c859037508e8e9222631bd618fff59812c47262a
Julian Cerruti <jcerruti@gmai.com>
2024-10-09 18:12:36 -0300

    last fixes

--- object_tracker_0/src/inference.py
+++ object_tracker_0/src/inference.py
@@ -21,7 +21,7 @@ import ffmpeg
 from PIL import Image
 from transformers import AutoProcessor, AutoModelForZeroShotObjectDetection
 from utils import Timer, my_device, start_experiment
-from datetime import datetime
+from datetime import datetime, timezone
 from sam2.sam2_video_predictor import SAM2VideoPredictor
 import numpy as np
 
@@ -237,7 +237,7 @@ def get_video_info(video_path: str) -> dict:
         date_string = media_details.get("tags", {}).get("creation_time", None)
         if date_string is None:
             return None
-        return datetime.strptime(date_string, "%Y-%m-%dT%H:%M:%S.%fZ")
+        return datetime.fromisoformat(date_string)
 
     video_info = {
         # Creation time, can be None if not available, otherwise we'll try to convert it to a timestamp
@@ -329,7 +329,7 @@ def perform_object_tracking(video_path, annotation_path, working_dir, frame_batc
             "video_timestamp": video_info['creation_time'].isoformat() if video_info['creation_time'] is not None else None,
             "fps": video_info['fps'],
             "description": "Object tracking results",
-            "inference_timestamp": datetime.now(tz=datetime.timezone.utc).isoformat(),
+            "inference_timestamp": datetime.now(tz=timezone.utc).isoformat(),
             "timings": f"Detection: {timer_total}, Inference: {timer_inference}"
         },
         'categories': [
