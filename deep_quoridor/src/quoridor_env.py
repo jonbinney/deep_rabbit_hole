53edaa816a80803ecf86aca231cea877a4f68995
Julian Cerruti <jcerruti@gmai.com>
2025-03-07 12:28:17 -0300

    Address various review comments

--- deep_quoridor/src/quoridor_env.py
+++ deep_quoridor/src/quoridor_env.py
@@ -6,25 +6,29 @@ import numpy as np
 class QuoridorEnv(AECEnv):
     metadata = {"render_modes": ["ansi"], "name": "quoridor_v0"}
 
-    def __init__(self):
+    def __init__(self, board_size: int = 9, max_walls: int = 10):
         super().__init__()
 
         self.render_mode = "ansi"
 
-        self.board_size = 9  # 9x9 grid
-        self.wall_size = self.board_size - 1  # 8x8 grid for walls
-        self.max_walls = 10  # Each player gets 10 walls
+        self.board_size = board_size  # assumed square grid
+        self.wall_size = self.board_size - 1  # grid for walls
+        self.max_walls = max_walls  # Each player gets 10 walls
 
         self.possible_agents = ["player_1", "player_2"]
         self.agent_order = self.possible_agents.copy()
         self.agent_selection = self.agent_order[0]
 
+        # NOTE: Observation and action spaces are only meant as metadata, they don't hold actual state
         self.observation_spaces = {
             agent: spaces.Dict(
                 {
+                    # For now: 0 = empty, 1 = player 1, 2 = player 2 in a board_size x board_size grid
                     "board": spaces.Box(0, 2, (self.board_size, self.board_size), dtype=np.int8),
-                    "walls": spaces.Box(0, 1, (self.wall_size, self.wall_size), dtype=np.int8),
+                    # For now: 0 = no wall, 1 = wall on a grid of wall_size x wall_size x orientation (0 = vertical, 1 = horizontal)
+                    "walls": spaces.Box(0, 1, (self.wall_size, self.wall_size, 2), dtype=np.int8),
                     "walls_remaining": spaces.Discrete(self.max_walls + 1),
+                    "action_mask": spaces.Box(0, 1, (self.board_size**2 + (self.wall_size**2) * 2,), dtype=np.int8),
                 }
             )
             for agent in self.possible_agents
@@ -37,13 +41,10 @@ class QuoridorEnv(AECEnv):
         self.reset()
 
     def reset(self, seed=None, options=None):
-        self.board = np.zeros((self.board_size, self.board_size), dtype=np.int8)
         self.walls = np.zeros((self.wall_size, self.wall_size, 2), dtype=np.int8)
         self.walls_remaining = {agent: self.max_walls for agent in self.possible_agents}
         # Positions are (row, col)
         self.positions = {"player_1": (4, 0), "player_2": (4, 8)}
-        self.board[self.positions["player_1"]] = 1
-        self.board[self.positions["player_2"]] = 2
 
         self.rewards = {agent: 0 for agent in self.possible_agents}
         self.terminations = {agent: False for agent in self.possible_agents}
@@ -85,9 +86,7 @@ class QuoridorEnv(AECEnv):
         row, col = self.idx_to_rowcol(action)
         if (row, col) in self.positions.values():
             return  # Invalid move (occupied)
-        self.board[self.positions[agent]] = 0
         self.positions[agent] = (row, col)
-        self.board[row, col] = 1 if agent == "player_1" else 2
 
     def _place_wall(self, agent, action):
         """
@@ -95,13 +94,14 @@ class QuoridorEnv(AECEnv):
         In a 9x9 board:
         - First we subtract 81 to get the wall index
         - The first 8x8 = 64 values reprsent vertical walls, the last 8x8 = 64 values represent horizontal walls
-        - For vertical walls, we place the wall starting atht upper right corner of the (row, col) cell
+        - For vertical walls, we place the wall starting at the upper right corner of the (row, col) cell
         - For horizontal walls, we place the wall starting at the lower left corner of the (row, col) cell
 
         TODO: Prevent placing walls conflicting with existing ones
         TODO: Prevent completely blocking the path from one end to the other
         """
         if self.walls_remaining[agent] == 0:
+            print("WTF: Invalid action: No walls left")
             return  # No walls left
         wall_idx = action - self.board_size**2
         orientation, wall_idx = divmod(wall_idx, self.wall_size**2)
