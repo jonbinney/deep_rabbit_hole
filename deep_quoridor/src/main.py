8eb9f876a224b59d95c4ef845d5f858b1a9206d2
Alejandro Marcu <amarcu@gmail.com>
2025-03-12 13:38:28 -0700

    Allow to choose the rendering mode, as well as board size and max walls using program args

--- deep_quoridor/src/main.py
+++ deep_quoridor/src/main.py
@@ -1,49 +1,71 @@
 import curses
+import argparse
 
 from quoridor_env import env  # Import the environment from your file
 
-# Initialize environment
-game = env()
-game.reset()
 
-# Print initial board
-print("Initial Board State:")
-board = game.render()
-print(board)
+def play(board_size: int | None, max_walls: int | None, render: str):
+    # Don't pass the None arguments to env so it uses the defaults
+    args = {"board_size": board_size, "max_walls": max_walls}
+    args = {k: v for k, v in args.items() if v is not None}
+    game = env(**args)
 
-step = 0
-stdscr = curses.initscr()
-curses.noecho()
-curses.cbreak()
-curses.start_color()
+    game.reset()
 
-for agent in game.agent_iter():
-    observation, reward, termination, truncation, info = game.last()
+    if render == "print":
+        print("Initial Board State:")
+        board = game.render()
+        print(board)
+    elif render == "curses":
+        stdscr = curses.initscr()
+        curses.noecho()
+        curses.cbreak()
+        curses.start_color()
 
-    mask = observation["action_mask"]
+    step = 0
 
-    if termination or truncation:
-        action = None
-        winner = max(game.rewards, key=game.rewards.get)
-        print(f"\nGame Over! {winner} wins.")
-        break
-    else:
-        # Hardcoded actions for now
-        action = game.action_space(agent).sample(mask)
-        print(f"\nStep {step + 1}: {agent} takes action {action}")
+    for agent in game.agent_iter():
+        observation, reward, termination, truncation, info = game.last()
 
-    game.step(action)  # Apply action
+        mask = observation["action_mask"]
 
-    board = game.render()
-    stdscr.clear()
-    stdscr.addstr(0, 0, board)
-    stdscr.refresh()
-    curses.napms(500)
-    print(board)
+        if termination or truncation:
+            action = None
+            winner = max(game.rewards, key=game.rewards.get)
+            print(f"\nGame Over! {winner} wins.")
+            break
+        else:
+            # Hardcoded actions for now
+            action = game.action_space(agent).sample(mask)
+            print(f"\nStep {step + 1}: {agent} takes action {action}")
 
-    step += 1
+        game.step(action)  # Apply action
 
-game.close()
-curses.nocbreak()
-curses.echo()
-curses.endwin()
+        board = game.render()
+
+        if render == "curses":
+            stdscr.clear()
+            stdscr.addstr(2, 2, board)
+            stdscr.refresh()
+            curses.napms(500)
+
+        print(board)
+
+        step += 1
+
+    if render == "curses":
+        curses.nocbreak()
+        curses.echo()
+        curses.endwin()
+
+    game.close()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Deep Quoridor")
+    parser.add_argument("-N", "--board_size", type=int, default=None, help="Board Size")
+    parser.add_argument("-W", "--max_walls", type=int, default=None, help="Max walls per player")
+    parser.add_argument("-r", "--render", choices=["print", "curses"], default="curses", help="Render mode")
+
+    args = parser.parse_args()
+    play(args.board_size, args.max_walls, args.render)
