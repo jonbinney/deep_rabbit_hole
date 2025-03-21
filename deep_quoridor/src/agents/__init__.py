6cccae0b2879daa8d35b55941abeefc1065127b8
Diego Belfer <dbelfer@gmail.com>
2025-03-21 23:22:54 +0000

    Moving to __init__.py to follow python style

--- deep_quoridor/src/agents/__init__.py
+++ deep_quoridor/src/agents/__init__.py
@@ -1,5 +1,48 @@
-from agents.agent import Agent  # noqa: F401
-from agents.agent_registry import AgentRegistry  # noqa: F401
-from agents.agent_registry import SelfRegisteringAgent  # noqa: F401
-from agents.random import RandomAgent  # noqa: F401
-from agents.simple import SimpleAgent  # noqa: F401
+class Agent:
+    """
+    Base class for all agents.
+    Given a game state, the agent should return an action.
+    """
+
+    def name(self) -> str:
+        raise NotImplementedError("You must implement the name method")
+
+    def get_action(self, game) -> int:
+        raise NotImplementedError("You must implement the get_action method")
+
+
+class AgentRegistry:
+    agents = {}
+
+    @staticmethod
+    def create(friendly_name: str) -> Agent:
+        return AgentRegistry.agents[friendly_name]()
+
+    @staticmethod
+    def names():
+        return list(AgentRegistry.agents.keys())
+
+    @staticmethod
+    def register(name: str, agent_class):
+        AgentRegistry.agents[name] = agent_class
+
+
+class SelfRegisteringAgent(Agent):
+    """
+    Base class for all agents.
+    Given a game state, the agent should return an action.
+    """
+
+    def __init_subclass__(cls, **kwargs):
+        AgentRegistry.register(SelfRegisteringAgent._friendly_name(cls.__name__), cls)
+
+    def name(self):
+        return SelfRegisteringAgent._friendly_name(self.__class__.__name__)
+
+    @staticmethod
+    def _friendly_name(class_name: str):
+        return class_name.replace("Agent", "").lower()
+
+
+from agents.random import RandomAgent  # noqa: E402, F401
+from agents.simple import SimpleAgent  # noqa: E402, F401
