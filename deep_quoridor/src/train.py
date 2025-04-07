c70189a0b29b7b3195c1174e5ff8306a85190c72
Diego Belfer <dbelfer@gmail.com>
2025-04-07 17:23:30 +0100

    using python lists

--- deep_quoridor/src/train.py
+++ deep_quoridor/src/train.py
@@ -1,6 +1,5 @@
 import argparse
 
-import numpy as np
 import torch
 from agents.core.agent import AgentRegistry
 from arena import Arena
@@ -43,7 +42,7 @@ def train_dqn(
         board_size=board_size,
         max_walls=max_walls,
         step_rewards=step_rewards,
-        renderers=np.concatenate([[print_plugin], renderers]),
+        renderers=[print_plugin] + renderers,
         plugins=plugins,
         swap_players=True,
     )
