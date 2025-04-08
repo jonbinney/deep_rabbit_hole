62f2d2b73a713fc9194874467acc8f486aefe23b
Julian Cerruti <jcerruti@gmai.com>
2025-04-07 21:33:42 -0300

    Implement Arena agent using SB3 PPO

--- deep_quoridor/src/agents/__init__.py
+++ deep_quoridor/src/agents/__init__.py
@@ -24,6 +24,7 @@ from agents.flat_dqn import FlatDQNAgent, Pretrained01FlatDQNAgent  # noqa: E402
 from agents.greedy import GreedyAgent  # noqa: E402, F401
 from agents.random import RandomAgent  # noqa: E402, F401
 from agents.replay import ReplayAgent  # noqa: E402, F401
+from agents.sb3_ppo import SB3PPOAgent  # noqa: E402, F401
 from agents.simple import SimpleAgent  # noqa: E402, F401
 
 AgentRegistry.register("dexppretrained", DExpPretrainedAgent)
@@ -31,3 +32,4 @@ AgentRegistry.register("pretrained01flatdqn", Pretrained01FlatDQNAgent)
 AgentRegistry.register("greedy", GreedyAgent)
 AgentRegistry.register("random", RandomAgent)
 AgentRegistry.register("simple", SimpleAgent)
+AgentRegistry.register("sb3ppo", SB3PPOAgent)
