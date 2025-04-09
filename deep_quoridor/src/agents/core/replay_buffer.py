e83e48fe356c2788b6004186a7ba400595183152
Diego Belfer <dbelfer@gmail.com>
2025-04-10 00:02:04 +0100

    other changes

--- deep_quoridor/src/agents/core/replay_buffer.py
+++ deep_quoridor/src/agents/core/replay_buffer.py
@@ -10,7 +10,7 @@ class ReplayBuffer:
 
     def add(self, state, action, reward, next_state, done):
         self.buffer.append([state, action, reward, next_state, done])
-        if reward >= 1:
+        if reward >= 10:
             self.buffer.append([state, action, reward, next_state, done])
             self.buffer.append([state, action, reward, next_state, done])
             self.buffer.append([state, action, reward, next_state, done])
