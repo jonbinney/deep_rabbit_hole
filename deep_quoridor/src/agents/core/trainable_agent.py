588d7252003093dc243f9dbffbab0dff2f11af3c
Diego Belfer <dbelfer@gmail.com>
2025-04-30 17:23:40 +0100

    gamecount not beign updated, not updating target network

--- deep_quoridor/src/agents/core/trainable_agent.py
+++ deep_quoridor/src/agents/core/trainable_agent.py
@@ -128,11 +128,11 @@ class AbstractTrainableAgent(Agent):
         self.train_call_losses = []
         self.reset_episode_related_info()
         self.resolve_and_load_model()
+        self.games_count = 0
 
     def reset_episode_related_info(self):
         self.current_episode_reward = 0
         self.player_id = None
-        self.games_count = 0
         self.steps = 0
 
     def is_trainable(self):
@@ -378,6 +378,7 @@ class AbstractTrainableAgent(Agent):
         target_q_values = self._compute_target_q_values(rewards, next_states, dones, next_state_masks)
 
         loss = self._update_network(current_q_values, target_q_values)
+        next_current_q_values = self._compute_current_q_values(states, actions)
         return loss
 
     def _prepare_batch(self, batch):
