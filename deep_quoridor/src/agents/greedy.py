d26b3776bc89211adbcab7552aa92bd76f0d7ca0
Diego Belfer <dbelfer@gmail.com>
2025-05-01 18:29:05 +0100

    revert greeedy

--- deep_quoridor/src/agents/greedy.py
+++ deep_quoridor/src/agents/greedy.py
@@ -4,6 +4,15 @@ from queue import Queue
 from typing import Optional, TypeAlias
 
 import numpy as np
+from quoridor import (
+    ActionEncoder,
+    MoveAction,
+    Player,
+    Quoridor,
+    WallAction,
+    WallOrientation,
+    construct_game_from_observation,
+)
 from utils import SubargsBase
 
 from agents.core import Agent
@@ -21,6 +30,10 @@ class GreedyAgent(Agent):
     def __init__(self, params=GreedyParams(), **kwargs):
         super().__init__()
         self.params = params
+        self.action_space = kwargs["action_space"]
+        self.board_size = kwargs["board_size"]
+        self.wall_size = self.board_size - 1
+        self.action_encoder = ActionEncoder(self.board_size)
 
     @classmethod
     def params_class(cls):
@@ -34,16 +47,16 @@ class GreedyAgent(Agent):
     def start_game(self, game, player_id):
         self.player_id = player_id
 
-    def _valid_pawn_movements(self, game, action_mask: np.ndarray) -> list[Position]:
+    def _valid_pawn_movements(self, action_mask: np.ndarray) -> list[Position]:
         """Returns the coordinates of the possible movements based on the action mask.
         This is useful for the first move, since there could be complicated jumps.  After the
         first move, you can't easily use this because you'd need to generate a new action_mask
         """
-        movement_mask = action_mask[: game.board_size**2]
+        movement_mask = action_mask[: self.board_size**2]
         valid_moves = np.argwhere(movement_mask == 1).reshape(-1)
-        return [game.action_index_to_params(i)[:2] for i in valid_moves]
+        return [self.action_encoder.index_to_action(i).destination for i in valid_moves]
 
-    def _next_moves(self, game, pos: Position) -> list[Position]:
+    def _next_moves(self, game: Quoridor, pos: Position) -> list[Position]:
         """Given a position in the board, it returns the possible next moves.  Notice that it ignores
         the other pawn, because this function is meant to be used for moves other than the first one,
         so the pawn may not be there anymore.
@@ -52,19 +65,19 @@ class GreedyAgent(Agent):
         for dr, dc in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
             row = pos[0] + dr
             col = pos[1] + dc
-            if game.is_in_board(row, col) and not game.is_wall_between(pos[0], pos[1], row, col):
+            if game.board.is_position_on_board((row, col)) and not game.board.is_wall_between(pos, (row, col)):
                 moves.append((row, col))
         return moves
 
-    def _shortest_path_from(self, game, pos: Position, target_row: int) -> list[Position]:
+    def _shortest_path_from(self, game: Quoridor, pos: Position, target_row: int) -> list[Position]:
         """
         Given a position in the board, it returns the shortest path to get to the target_row.
         """
         if pos[0] == target_row:
-            return []
+            return [pos]
 
         # How many steps are needed to get to that position, starting from pos (None means unreachable)
-        distances = [[None] * game.board_size for _ in range(game.board_size)]
+        distances = [[None] * game.board.board_size for _ in range(game.board.board_size)]
 
         # Queue for the BFS, containing the neighbords to process
         q = Queue()
@@ -92,51 +105,51 @@ class GreedyAgent(Agent):
 
         # Trace back the path
         path = [dest]
-        while True:
+        prev_pos = None
+        while prev_pos != pos:
             last_pos = path[-1]
             prev_pos = coming_from[last_pos]
-            if prev_pos == pos:
-                break
-
             path.append(prev_pos)
 
         return path[::-1]
 
-    def _shortest_path_from_me(self, game, observation: dict, target_row: int) -> list[Position]:
+    def _shortest_path_from_me(
+        self, game: Quoridor, player: Player, action_mask: np.ndarray, target_row: int
+    ) -> list[Position]:
         """Return the shortest path from wherever the agent currently is."""
-        coords = np.argwhere(observation["observation"]["board"] == 1)
-        my_position = (int(coords[0][0]), int(coords[0][1]))
+        my_position = game.board.get_player_position(player)
+
         # The first move is based on the action mask, that already calculated what positions are valid
-        first_moves = self._valid_pawn_movements(game, observation["action_mask"])
+        first_moves = self._valid_pawn_movements(action_mask)
         shortest_path = None
 
         # Find the shortest path for each of the first moves and keep the shortest one.
         for move in first_moves:
             path = self._shortest_path_from(game, move, target_row)
-            if shortest_path is None or (len(path) + 1) < len(shortest_path):
-                shortest_path = [my_position] + [move] + path
+            if shortest_path is None or len(path) < len(shortest_path):
+                shortest_path = [my_position] + path
 
         assert shortest_path is not None, "No path found"
         return shortest_path
 
-    def _get_opponent_position(self, game, board: np.ndarray) -> Position:
+    def _get_opponent_position(self, game: Quoridor, board: np.ndarray) -> Position:
         coords = np.argwhere(board == 2)
         assert len(coords) == 1, "Expected exactly one opponent position"
         return (int(coords[0][0]), int(coords[0][1]))
 
-    def _get_block_action(self, game, opponent_shortest_path: list[Position], action_mask: np.ndarray):
+    def _get_block_action(self, game: Quoridor, opponent_shortest_path: list[Position], action_mask: np.ndarray):
         for p0, p1 in zip(opponent_shortest_path, opponent_shortest_path[1:]):
             # For this movement of the opponent, get the possible wall placements
             if p0[0] == p1[0]:
                 # Horizontal movement
                 rows = [p0[0], p0[0] - 1]
                 cols = [min(p0[1], p1[1])]
-                orientation = 1
+                orientation = WallOrientation.VERTICAL
             elif p0[1] == p1[1]:
                 # Vertical movement
                 rows = [min(p0[0], p1[0])]
                 cols = [p0[1], p0[1] - 1]
-                orientation = 2
+                orientation = WallOrientation.HORIZONTAL
             else:
                 assert "Expected horizontal or vertical movement"
 
@@ -145,16 +158,21 @@ class GreedyAgent(Agent):
             # by looking into which makes the difference of the distances more favorable
             for r in rows:
                 for c in cols:
-                    if r < 0 or c < 0 or r >= game.wall_size or c >= game.wall_size:
+                    if r < 0 or c < 0 or r >= self.wall_size or c >= self.wall_size:
                         continue
-                    action = game.action_params_to_index(r, c, orientation)
+
+                    action = self.action_encoder.action_to_index(WallAction((r, c), orientation))
                     if action_mask[action] == 1:
                         return action
         # No actions found
         return None
 
     def _log_action(
-        self, game, observation: dict, my_shortest_path: list[Position], opponent_shortest_path: list[Position]
+        self,
+        observation: dict,
+        action_mask: np.ndarray,
+        my_shortest_path: list[Position],
+        opponent_shortest_path: list[Position],
     ):
         if not self.action_log.is_enabled():
             return
@@ -162,40 +180,39 @@ class GreedyAgent(Agent):
         self.action_log.clear()
 
         # Log the possible next movements
-        movement_mask = observation["action_mask"][: game.board_size**2]
+        movement_mask = action_mask[: self.board_size**2]
         for action in np.argwhere(movement_mask == 1).reshape(-1):
-            self.action_log.action_text(game.action_index_to_params(int(action)), "")
+            self.action_log.action_text(self.action_encoder.index_to_action(int(action)), "")
 
-        my_coords = np.argwhere(observation["observation"]["board"] == 1)
-        path = my_shortest_path[:]
-        path.insert(0, (int(my_coords[0][0]), int(my_coords[0][1])))
-        self.action_log.path(path)
+        self.action_log.path(my_shortest_path)
+        self.action_log.path(opponent_shortest_path)
 
-        opp_coords = np.argwhere(observation["observation"]["board"] == 2)
-        path = opponent_shortest_path[:]
-        path.insert(0, (int(opp_coords[0][0]), int(opp_coords[0][1])))
-        self.action_log.path(path)
+        my_pos = MoveAction(my_shortest_path[0])
+        opp_pos = MoveAction(opponent_shortest_path[0])
 
-    def get_action(self, game):
-        observation, _, termination, truncation, _ = game.last()
-        if termination or truncation:
-            return None
+        self.action_log.action_text(my_pos, f"dist:{len(my_shortest_path) - 1}")
+        self.action_log.action_text(opp_pos, f"dist: {len(opponent_shortest_path) - 1}")
+
+    def get_action(self, observation, action_mask):
+        # Reconstruct the game from the observation.
+        game, player, opponent = construct_game_from_observation(observation, self.player_id)
 
         if random.random() < self.params.p_random:
             if self.action_log.is_enabled():
                 self.action_log.clear()
-                # TO DO: when we have the functionality in the log to output a message,
-                # use it to say that the move will be random.
-            return game.action_space(game.agent_selection).sample(observation["action_mask"])
 
-        goal_row = game.board_size - 1 if self.player_id == "player_0" else 0
-        opponent_row = game.board_size - 1 - goal_row
+            my_pos = MoveAction(game.board.get_player_position(player))
+            self.action_log.action_text(my_pos, "RAND")
+            return self.action_space.sample(action_mask)
 
-        my_shortest_path = self._shortest_path_from_me(game, observation, goal_row)
-        opponent_pos = self._get_opponent_position(game, observation["observation"]["board"])
-        opponent_shortest_path = [opponent_pos] + self._shortest_path_from(game, opponent_pos, opponent_row)
+        goal_row = game.get_goal_row(player)
+        opponent_goal_row = game.get_goal_row(opponent)
 
-        self._log_action(game, observation, my_shortest_path, opponent_shortest_path)
+        my_shortest_path = self._shortest_path_from_me(game, player, action_mask, goal_row)
+        opponent_pos = game.board.get_player_position(opponent)
+        opponent_shortest_path = self._shortest_path_from(game, opponent_pos, opponent_goal_row)
+
+        self._log_action(observation, action_mask, my_shortest_path, opponent_shortest_path)
 
         # TODO: use a more elaborate logic to decide whether to block, could be probabilistic
         block = False
@@ -203,8 +220,8 @@ class GreedyAgent(Agent):
             if len(opponent_shortest_path) < 5:
                 block = True
         if block:
-            action = self._get_block_action(game, opponent_shortest_path, observation["action_mask"])
+            action = self._get_block_action(game, opponent_shortest_path, action_mask)
             if action is not None:
                 return action
 
-        return game.action_params_to_index(my_shortest_path[1][0], my_shortest_path[1][1], 0)
+        return self.action_encoder.action_to_index(MoveAction((my_shortest_path[1][0], my_shortest_path[1][1])))
