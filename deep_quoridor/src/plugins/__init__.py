226f30ac7ae5e89619c99d1282d2677e103df1a2
Alejandro Marcu <amarcu@gmail.com>
2025-05-05 15:43:07 -0700

    Improvements for wandb setup

--- deep_quoridor/src/plugins/__init__.py
+++ deep_quoridor/src/plugins/__init__.py
@@ -1,3 +1,3 @@
 from plugins.arena_yaml_recorder import ArenaYAMLRecorder  # noqa: E402, F401
 from plugins.save_model import SaveModelEveryNEpisodesPlugin  # noqa: E402, F401
-from plugins.wandb_train import WandbTrainPlugin  # noqa: E402, F401
+from plugins.wandb_train import WandbParams, WandbTrainPlugin  # noqa: E402, F401
