ba9a36cd7f2b33ccd0c552378ae2502be9e7b4d2
Jon Binney <jon.binney@gmail.com>
2025-07-06 12:24:52 -0400

    Test rotation with walls and moves

--- deep_quoridor/test/agents/alphazero_test.py
+++ deep_quoridor/test/agents/alphazero_test.py
@@ -1,7 +1,7 @@
 import numpy as np
 from agents.alphazero.nn_evaluator import NNEvaluator
 from agents.core import rotation
-from quoridor import ActionEncoder, Board, Quoridor
+from quoridor import ActionEncoder, Board, MoveAction, Quoridor, WallAction, WallOrientation
 from utils import my_device
 
 
@@ -16,10 +16,7 @@ def _assert_policy_and_rotated_policy_equivalent(
         assert policy[action_index] == rotated_policy[roated_action_index]
 
 
-def _rotation_test_setup(board_size: int, max_walls) -> tuple[ActionEncoder, NNEvaluator, Quoridor]:
-    board_size = 3
-    max_walls = 1
-
+def _evaluator_rotation_test_setup(board_size: int, max_walls) -> tuple[ActionEncoder, NNEvaluator, Quoridor]:
     action_encoder = ActionEncoder(board_size)
     evaluator = NNEvaluator(action_encoder, my_device())
     game = Quoridor(Board(board_size, max_walls))
@@ -27,8 +24,40 @@ def _rotation_test_setup(board_size: int, max_walls) -> tuple[ActionEncoder, NNE
     return action_encoder, evaluator, game
 
 
-def test_evaluator_game_rotation():
-    action_encoder, evaluator, game_1 = _rotation_test_setup(board_size=3, max_walls=1)
+def test_evaluator_rotation_on_initial_game():
+    action_encoder, evaluator, game_1 = _evaluator_rotation_test_setup(board_size=3, max_walls=0)
+
+    game_2 = game_1.copy()
+    game_2.go_to_next_player()
+    value_1, policy_1 = evaluator.evaluate(game_1)
+    value_2, policy_2 = evaluator.evaluate(game_2)
+    assert value_1 == value_2
+    _assert_policy_and_rotated_policy_equivalent(action_encoder, policy_1, policy_2)
+
+
+def test_evaluator_rotation_with_players_in_symmetrical_positions():
+    action_encoder, evaluator, game_1 = _evaluator_rotation_test_setup(board_size=5, max_walls=0)
+
+    game_1.step(MoveAction((1, 0)), validate=False)
+    game_1.step(MoveAction((3, 4)), validate=False)
+
+    game_2 = game_1.copy()
+    game_2.go_to_next_player()
+    value_1, policy_1 = evaluator.evaluate(game_1)
+    value_2, policy_2 = evaluator.evaluate(game_2)
+    assert value_1 == value_2
+    _assert_policy_and_rotated_policy_equivalent(action_encoder, policy_1, policy_2)
+
+
+def test_evaluator_rotation_with_players_and_walls_in_symmetrical_positions():
+    action_encoder, evaluator, game_1 = _evaluator_rotation_test_setup(board_size=5, max_walls=2)
+
+    game_1.step(MoveAction((1, 0)), validate=False)
+    game_1.step(MoveAction((3, 4)), validate=False)
+    game_1.step(WallAction((0, 1), WallOrientation.HORIZONTAL))
+    game_1.step(WallAction((3, 2), WallOrientation.HORIZONTAL))
+    game_1.step(WallAction((2, 0), WallOrientation.VERTICAL))
+    game_1.step(WallAction((1, 3), WallOrientation.VERTICAL))
 
     game_2 = game_1.copy()
     game_2.go_to_next_player()
