4d5acbfa51c1b68501cebea0bf553264da1157ef
Jon Binney <jon.binney@gmail.com>
2025-07-22 11:26:21 -0400

    Experiment with parallel MCTS
    I'm going to end up rolling this back. The problem I ran into is that even if you expand different nodes in the tree, if they have any shared parents you really need to expand them sequentially or else you'll use the wrong visit counts and values for the second one.

--- deep_quoridor/src/agents/alphazero/mcts.py
+++ deep_quoridor/src/agents/alphazero/mcts.py
@@ -7,18 +7,21 @@ from quoridor import Action, Quoridor
 class Node:
     def __init__(
         self,
-        game: Optional[Quoridor],  # If None, the game will be computed lazily from the parent when needed
+        game: Optional[Quoridor] = None,  # If None, the game will be computed lazily from the parent when needed
         parent: Optional["Node"] = None,
         action_taken: Optional[Action] = None,
         ucb_c: float = 1.0,
         prior: float = 0.0,
+        node_id: tuple[int] = (),  # Sequence of action id's to go from root to here, which acts as a unique id
     ):
         if game is None:
+            assert node_id is not None, "When constructing a node without a game, the node_id must be provided"
             assert parent is not None, "When constructing a node without a game, the parent should be provided"
             assert action_taken is not None, (
                 "When constructing a node without a game, the action_taken should be provided"
             )
 
+        self.node_id = node_id
         self._game = game
         self.parent = parent
         self.action_taken = action_taken
@@ -29,6 +32,12 @@ class Node:
 
         self.ucb_c = ucb_c
         self.prior = prior
+
+    def __hash__(self):
+        return hash(self.node_id)
+
+    def __eq__(self, other):
+        return self.node_id == other.node_id
 
     @property
     def game(self) -> Quoridor:
@@ -54,13 +63,22 @@ class Node:
             assert 0.0 <= prob <= 1.0, "Invalid action probability in policy"
             if prob > 0.0:
                 action = self.game.action_encoder.index_to_action(action_index)
-                child = Node(game=None, parent=self, action_taken=action, ucb_c=self.ucb_c, prior=prob)
+                child_node_id = self.node_id + (action_index,)
+                child = Node(
+                    node_id=child_node_id, game=None, parent=self, action_taken=action, ucb_c=self.ucb_c, prior=prob
+                )
                 self.children.append(child)
 
     def select(self) -> "Node":
         """
         Return the child of the current node with the highest ucb
         """
+        child_ucbs = self.get_child_ucbs()
+        best_child_i = np.argmax(child_ucbs)
+
+        return self.children[best_child_i]
+
+    def get_child_ucbs(self):
         ucbc_visitcount = self.ucb_c * np.sqrt(self.visit_count)
 
         def get_child_ucb(child: "Node") -> float:
@@ -72,7 +90,7 @@ class Node:
                 q_value = (child.value_sum / child.visit_count + 1) / 2
             return q_value + child.prior * ucbc_visitcount / (child.visit_count + 1)
 
-        return max(self.children, key=get_child_ucb)
+        return [get_child_ucb(child) for child in self.children]
 
     def backpropagate(self, value: float):
         """
@@ -102,7 +120,7 @@ class MCTS:
         return node
 
     def search(self, initial_game: Quoridor):
-        root = Node(initial_game, ucb_c=self.ucb_c)
+        root = Node(game=initial_game, ucb_c=self.ucb_c)
 
         for _ in range(self.n):
             # Traverse down the tree guided by maximum UCB until we find a node to expand
@@ -119,3 +137,47 @@ class MCTS:
             node.backpropagate(value)
 
         return root.children
+
+    def batch_search(self, initial_game: Quoridor, batch_size: int):
+        root = Node(initial_game, ucb_c=self.ucb_c)
+
+        batch = set()
+        nodes_to_search = [root] * self.n
+        while len(nodes_to_search) > 0:
+            # Traverse down the tree guided by maximum UCB until we find a node to expand
+            start_node = nodes_to_search.pop()
+            node = self.select(start_node)
+
+            if node.game.is_game_over():
+                # The player who just made a move must have won.
+                value = 1
+                node.backpropagate(value)
+            # TODO: Handle ties (these happen when arena decides game is taking too long)
+            elif node in batch:
+                # If we keep finding the same node when searching, e.g. if we have only the root
+                # node in the tree or one search path is very likely, we could end up in a deadlock
+                # where we never get a batch big enough to expand. To avoid this, when we see a node
+                # already in the batch, we immediately expand the entire batch and put that node back
+                # on the stack (we found it twice; the first time resulted in the expand and the second
+                # select needs to be finished now that the node is expanded.
+                self.expand_batch(list(batch))
+                batch.clear()
+                nodes_to_search.append(node)
+            elif len(batch) >= batch_size:
+                # Batch is large enough, expand the nodes
+                self.expand_batch(list(batch))
+                batch.clear()
+            else:
+                batch.add(node)
+
+        return root.children
+
+    def expand_batch(self, batch: list[Node]):
+        print(f"Batch size: {len(batch)}")
+        values, policies = self.evaluator.evaluate_batch([node.game for node in batch])
+        for node, value, policy in zip(batch, values, policies):
+            print(
+                f"Expanding {(batch[0].node_id)}: visit_count={batch[0].visit_count} num_children={len(batch[0].children)}"
+            )
+            node.expand(policy)
+            node.backpropagate(value)
