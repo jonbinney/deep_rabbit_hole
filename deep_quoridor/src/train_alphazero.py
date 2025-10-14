89c656a487dc5d3a6a2f561232862d185641bc95
Alejandro Marcu <amarcu@gmail.com>
2025-10-13 20:21:47 -0700

    Cliff -> AgentEvolution

--- deep_quoridor/src/train_alphazero.py
+++ deep_quoridor/src/train_alphazero.py
@@ -6,10 +6,10 @@ import time
 from dataclasses import asdict
 from typing import Optional
 
+from agent_evolution_tournament import AgentEvolutionTournament, AgentEvolutionTournamentParams
 from agents.alphazero.alphazero import AlphaZeroAgent, AlphaZeroBenchmarkOverrideParams, AlphaZeroParams
 from agents.alphazero.self_play_manager import GameParams, SelfPlayManager
 from agents.core.agent import AgentRegistry
-from cliff_tournament import CliffTournament, CliffTournamentParams
 from metrics import Metrics
 from plugins.wandb_train import WandbParams, WandbTrainPlugin
 from utils import Timer, parse_subargs, set_deterministic
@@ -134,15 +134,24 @@ def main(args):
 
             agent_encoded_name = "alphazero:" + override_subargs(args.params, override_args)
 
-        if args.cliff is not None:
-            cliff_params = parse_subargs(args.cliff, CliffTournamentParams)
-            assert isinstance(cliff_params, CliffTournamentParams)
-            cliff_tournament = CliffTournament(args.board_size, args.max_walls, args.max_steps, cliff_params)
+        if args.agent_evolution is not None:
+            agent_evolution_params = parse_subargs(args.agent_evolution, AgentEvolutionTournamentParams)
+            assert isinstance(agent_evolution_params, AgentEvolutionTournamentParams)
+            agent_evolution_tournament = AgentEvolutionTournament(
+                args.board_size,
+                args.max_walls,
+                args.max_steps,
+                agent_evolution_params,
+            )
         else:
-            cliff_tournament = None
+            agent_evolution_tournament = None
 
         wandb_train_plugin = WandbTrainPlugin(
-            wandb_params, args.epochs * args.games_per_epoch, agent_encoded_name, metrics, cliff_tournament
+            wandb_params,
+            args.epochs * args.games_per_epoch,
+            agent_encoded_name,
+            metrics,
+            agent_evolution_tournament,
         )
 
     t0 = time.time()
@@ -216,7 +225,13 @@ if __name__ == "__main__":
         help="How many games to play in parallel per process",
     )
     parser.add_argument(
-        "-c", "--cliff", nargs="?", const="", default=None, type=str, help="Parameters for the Cliff Tournament"
+        "-a",
+        "--agent_evolution",
+        nargs="?",
+        const="",
+        default=None,
+        type=str,
+        help="Parameters for the Agent Evolution Tournament",
     )
     args = parser.parse_args()
 
