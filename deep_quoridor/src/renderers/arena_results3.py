178a27d3972a7394b5e18c845d9a5798990d5df3
Jon Binney <jon.binney@gmail.com>
2025-12-27 16:50:43 -0500

    Show both P1 and P2 results in ArenaResults3

--- deep_quoridor/src/renderers/arena_results3.py
+++ deep_quoridor/src/renderers/arena_results3.py
@@ -1,5 +1,3 @@
-from collections import defaultdict
-
 from arena import GameResult
 from prettytable import PrettyTable
 
@@ -24,7 +22,11 @@ class ArenaResults3Renderer(Renderer):
         all_players = sorted(set([r.player1 for r in results]) | set([r.player2 for r in results]))
         players = {player: i for i, player in enumerate(all_players)}
 
-        games = defaultdict(lambda: {"wins": 0, "losses": 0, "ties": 0})
+        games = dict()
+        for p1 in players:
+            for p2 in players:
+                if p2 != p1:
+                    games[(p1, p2)] = {"wins": 0, "losses": 0, "ties": 0}
 
         for r in results:
             matchup = (r.player1, r.player2)
@@ -40,21 +42,27 @@ class ArenaResults3Renderer(Renderer):
         table = PrettyTable()
         table.field_names = ["Matchup", "Wins", "Losses", "Ties"]
 
-        for player1 in players:
-            table.add_row([f"P1 {player1}", "", "", ""])
-
-            for player2 in players:
-                matchup = (player1, player2)
-                if matchup not in games:
-                    continue
-
+        def add_row(games, matchup, for_p2=False):
+            if matchup in games:
+                wins_key = "losses" if for_p2 else "wins"
+                losses_key = "wins" if for_p2 else "losses"
                 num_games = sum([n for n in games[matchup].values()])
-                win_perc = perc(games[matchup]["wins"], num_games)
-                loss_perc = perc(games[matchup]["losses"], num_games)
+                win_perc = perc(games[matchup][wins_key], num_games)
+                loss_perc = perc(games[matchup][losses_key], num_games)
                 tie_perc = perc(games[matchup]["ties"], num_games)
+                table.add_row([f"  vs {player_b}", win_perc, loss_perc, tie_perc])
 
-                table.add_row([f"  vs {player2}", win_perc, loss_perc, tie_perc])
+        for player_a in players:
+            table.add_row([f"P1 {player_a}", "", "", ""])
+            for player_b in players:
+                matchup = (player_a, player_b)
+                add_row(games, matchup)
+            table.add_row(["", "", "", ""])
 
+            table.add_row([f"P2 {player_a}", "", "", ""])
+            for player_b in players:
+                matchup = (player_b, player_a)
+                add_row(games, matchup, for_p2=True)
             table.add_row(["", "", "", ""])
 
         print(table)
