ccb1d22d4485e3c0a18581e50f38225fa90f136f
Jon Binney <jon.binney@gmail.com>
2026-01-28 20:24:46 -0500

    Show total win percentages in arenaresults3

--- deep_quoridor/src/renderers/arena_results3.py
+++ deep_quoridor/src/renderers/arena_results3.py
@@ -41,6 +41,7 @@ class ArenaResults3Renderer(Renderer):
 
         table = PrettyTable()
         table.field_names = ["Matchup", "Wins", "Losses", "Ties"]
+        table.align="l"
 
         def add_row(games, matchup, for_p2=False):
             if matchup in games:
@@ -52,17 +53,37 @@ class ArenaResults3Renderer(Renderer):
                 tie_perc = perc(games[matchup]["ties"], num_games)
                 table.add_row([f"  vs {player_b}", win_perc, loss_perc, tie_perc])
 
+        def add_total_row(games, player, for_p2=False):
+            wins_key = "losses" if for_p2 else "wins"
+            losses_key = "wins" if for_p2 else "losses"
+            num_wins = 0
+            num_losses= 0
+            num_ties = 0
+            for matchup in games:
+                if not for_p2 and matchup[0] == player or for_p2 and matchup[1] == player:
+                    num_wins += games[matchup][wins_key]
+                    num_losses+= games[matchup][losses_key]
+                    num_ties += games[matchup]["ties"]
+
+            num_games = num_wins + num_losses+ num_ties
+            win_perc = perc(num_wins, num_games)
+            loss_perc = perc(num_losses, num_games)
+            tie_perc = perc(num_ties, num_games)
+            table.add_row([f"  total", win_perc, loss_perc, tie_perc])
+
         for player_a in players:
             table.add_row([f"P1 {player_a}", "", "", ""])
             for player_b in players:
                 matchup = (player_a, player_b)
                 add_row(games, matchup)
+            add_total_row(games, player_a)
             table.add_row(["", "", "", ""])
 
             table.add_row([f"P2 {player_a}", "", "", ""])
             for player_b in players:
                 matchup = (player_b, player_a)
                 add_row(games, matchup, for_p2=True)
+            add_total_row(games, player_a, for_p2=True)
             table.add_row(["", "", "", ""])
 
         print(table)
